57a9cde0d161c110eefbfcdac0341be1907544fa
afif95 <afifbinkamrul@gmail.com>
2023-10-18 23:23:17 -0400

    redux-toolkit

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,34 +1,39 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-// reducer
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return { ...state, fullName: action.payload };
-    default:
-      return state;
-  }
-}
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      // prepare: function(fullName, nationalId) {
+      prepare(fullName, nationalId) {
+        return {
+          payload: {
+            fullName,
+            nationalId,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-// action creators
-export function CreateCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    // side effects should not be in reducer (new Date())
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-export function updateName(fullName) {
-  return { type: "customer/updateName", payload: fullName };
-}
+// in the main branch, createCustomer was CreateCustomer
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
